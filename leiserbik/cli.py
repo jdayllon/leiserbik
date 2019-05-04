e5eb3ebf16c79a2769c3ca046b118aaf635a82a9
Juan David <jdayllon@gmail.com>
2019-05-04 09:52:49 +0200

    Improvements on logging cli configuration

--- leiserbik/cli.py
+++ leiserbik/cli.py
@@ -14,25 +14,32 @@ import arrow
 cprint = pprint.PrettyPrinter(indent=4).pprint
 
 @click.group()
-@click.option('-d/-ns', '--debug/--no-debug', default=False)
+@click.option('-v', '--verbose', count=True)
 @click.option('-s/-ns', '--stream/--no-stream', default=False)
 @click.option('-w/-nw', '--write/--no-write', default=False)
 @click.pass_context
-def main(ctx, debug, stream, write):
-
-    if not debug:
+def main(ctx, verbose, stream, write):
+    if verbose == 0:
+        logger.info("🕵 ‍Logger level: WARNING")
         logger.remove()
         logger.add(sys.stderr, level="WARNING")
+    elif verbose == 1:
+        logger.info("🕵 ‍Logger level: INFO")
+        logger.remove()
+        logger.add(sys.stderr, level="INFO")
+    elif verbose == 2:
+        logger.info("🕵 ‍Logger level: DEBUG")
+        logger.remove()
+        logger.add(sys.stderr, level="DEBUG")
+    else:
+        logger.info("🕵 ‍Logger level: TRACE")
+        logger.remove()
+        logger.add(sys.stderr, level="TRACE")
 
     ctx.ensure_object(dict)
-    ctx.obj['DEBUG'] = debug
+
     ctx.obj['STREAM'] = stream
     ctx.obj['WRITE'] = write
-
-
-#@click.pass_context
-#def cli(ctx, debug):
-#    pass
 
 @main.command(context_settings=dict(allow_extra_args=True))
 @click.pass_context
@@ -40,7 +47,7 @@ def main(ctx, debug, stream, write):
 def user(ctx, screen_name):
     """Retrieves info about user. If no option is given string after command is identified with screen name requiered"""
 
-    loguru.info("Running User Extraction")
+    logger.info("Running User Extraction")
 
     if screen_name is None and len(ctx.args) == 1:
         screen_name = ctx.args[0]
@@ -56,7 +63,7 @@ def user(ctx, screen_name):
 @click.option('-s','--screen_name', help='Twitter Screen Name.')
 def query_user(ctx, screen_name):
     """Retrieves info about user. If no option is given string after command is identified with screen name requiered"""
-    loguru.info("Running User Query")
+    logger.info("Running User Query")
 
     if screen_name is None and len(ctx.args) == 1:
         screen_name = ctx.args[0]
@@ -132,10 +139,7 @@ def rawquery(ctx, query=None, end_date: str = arrow.get().shift(days=-1).format(
                 cprint(cur_status)
                 sys.stdout.flush()
 
-    #cprint(dir(ctx.args))
-    #cprint(ctx.args)
-
     return 0
 
 if __name__ == "__main__":
-    sys.exit(main(obj={}))  # pragma: no cover
+    sys.exit(main(obj={}))  # pylint: disable= all; problems with click call
